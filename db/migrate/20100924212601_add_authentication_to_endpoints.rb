3cd220d36f5b81d6f6a802bf8e0dcb5b3557cee2
Adam McDonald <amcdonald_3@yahoo.com>
2010-10-14 15:11:03 -0700

    updated schema

--- db/migrate/20100924212601_add_authentication_to_endpoints.rb
+++
@@ -1,9 +0,0 @@
-class AddAuthenticationToEndpoints < ActiveRecord::Migration
-  def self.up
-    add_column :endpoints, :authentication_id, :integer
-  end
-
-  def self.down
-    remove_column :endpoints, :authentication_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101014201118) do
+ActiveRecord::Schema.define(:version => 20101014181922) do
 
   create_table "authentications", :force => true do |t|
     t.string   "auth_method"
