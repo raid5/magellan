9ae0860ac5726c1d13a850d0733ecfb1f30ffff8
Adam McDonald <mcdonald.adam@gmail.com>
2011-01-11 17:29:14 -0800

    Disabled oauth selection when creating new auth schemes. Also, updated initial setup flow.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
     @global_params = GlobalParameter.all
     @groups = Group.order("name")
     
-    @explore_endpoint = params[:endpoint_id].blank? ? Endpoint.first : Endpoint.find(params[:endpoint_id])
+    @has_endpoints = Endpoint.exists?
+    #@explore_endpoint = params[:endpoint_id].blank? ? Endpoint.first : Endpoint.find(params[:endpoint_id])
   end
 end
