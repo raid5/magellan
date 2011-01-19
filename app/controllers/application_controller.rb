bd5776552ed353437450b93026ab9ea79ffabf8b
Adam McDonald <mcdonald.adam@gmail.com>
2011-01-18 18:54:17 -0800

    basic auth unprotection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
   # Uncomment to require HTTP Basic authentication for all actions except
   #  for exploring the API.
   
-  before_filter :authenticate
-  
-  def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'raid5' && password == '73raidferdmag'
-    end unless controller_name == 'endpoints' && action_name == 'show'
-  end
+  # before_filter :authenticate
+  # 
+  # def authenticate
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == 'raid5' && password == '73raidferdmag'
+  #   end unless controller_name == 'endpoints' && action_name == 'show'
+  # end
 end
