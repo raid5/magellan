659fb594e2aa8c5d1f74e86327d01154c3fd0ae3
Adam McDonald <mcdonald.adam@gmail.com>
2011-01-19 14:05:50 -0800

    Changed http basic auth scheme to use system environment vars

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   # Uncomment to require HTTP Basic authentication for all actions except
-  #  for exploring the API.
+  #  for exploring the API. Note: Make sure the two environment variables
+  #  MAG_USERNAME and MAG_PASSWORD are defined.
   
-  # before_filter :authenticate
-  # 
-  # def authenticate
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == 'admin' && password == 'password'
-  #   end unless controller_name == 'endpoints' && action_name == 'show'
-  # end
+  before_filter :authenticate
+  
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['MAG_USERNAME'] && password == ENV['MAG_PASSWORD']
+    end unless controller_name == 'endpoints' && action_name == 'show'
+  end
 end
