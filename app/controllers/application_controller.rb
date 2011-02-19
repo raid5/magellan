c31aa3f6b7ba6e28858faa363f24c960915d850c
Adam McDonald <mcdonald.adam@gmail.com>
2011-02-19 11:26:26 -0800

    heroku config.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
   #  for exploring the API. Note: Make sure the two environment variables
   #  MAG_USERNAME and MAG_PASSWORD are defined.
   
-  # before_filter :authenticate
-  # 
-  # def authenticate
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == ENV['MAG_USERNAME'] && password == ENV['MAG_PASSWORD']
-  #   end unless controller_name == 'endpoints' && (action_name == 'show' || action_name == 'explore')
-  # end
+  before_filter :authenticate
+  
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['MAG_USERNAME'] && password == ENV['MAG_PASSWORD']
+    end unless controller_name == 'endpoints' && (action_name == 'show' || action_name == 'explore')
+  end
 end
